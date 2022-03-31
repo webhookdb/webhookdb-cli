758349944ce506a88749946256adb37dfb02fd55
Natalie <natalie@lithic.tech>
2022-03-31 13:30:38 -0700

    Update CLI with new FDW url (#287)

--- client/organizations.go
+++ client/organizations.go
@@ -103,21 +103,3 @@ func OrgRename(c context.Context, auth Auth, input OrgRenameInput) (out types.Me
 	err = makeRequest(c, POST, auth, input, &out, "/v1/organizations/%v/rename", input.OrgIdentifier)
 	return
 }
-
-type OrgFdwInput struct {
-	OrgIdentifier    types.OrgIdentifier `json:"-"`
-	MessageFdw       bool                `json:"message_fdw"`
-	MessageViews     bool                `json:"message_views"`
-	MessageAll       bool                `json:"message_all"`
-	RemoteServerName string              `json:"remote_server_name"`
-	FetchSize        string              `json:"fetch_size"`
-	LocalSchema      string              `json:"local_schema"`
-	ViewSchema       string              `json:"view_schema"`
-}
-
-type OrgFdwOutput map[string]interface{}
-
-func OrgFdw(c context.Context, auth Auth, input OrgFdwInput) (out OrgFdwOutput, err error) {
-	err = makeRequest(c, POST, auth, input, &out, "/v1/organizations/%v/fdw", input.OrgIdentifier)
-	return
-}
